816bca0faaa7caf59891938fba109dc4c24b2236
Deni Baskovsky <qertis@users.noreply.github.com>
2022-08-21 21:47:31 +0300

    Создать договор на оказание работы #59

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-HTMLs=(index.html)
+set -o pipefail -e
+
+HTMLs=(index.html puboferta.html)
 for item in ${HTMLs[*]}
 do
   mobirise-optimizator $item --minifier=true --input-dir "./www/" --output-dir "www/"
 done
+
+set +o pipefail +e
 
 echo 'OK'
